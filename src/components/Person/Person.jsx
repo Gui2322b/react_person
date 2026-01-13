302d76f09f49d69b36df13fdafe1332755c65110
gui2322b <guiribeiro2322@gmail.com>
2026-01-12 23:44:57 -0300

    revised person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,18 +1,22 @@
-
 export const Person = ({ person }) => {
   // lógica para parceiro(a)
+  const { name, age, sex, isMarried, partnerName } = person;
   let partnerText = 'I am not married';
-  if (person.isMarried) {
-    partnerText = person.sex === 'm'
-      ? `${person.partnerName} is my wife`
-      : `${person.partnerName} is my husband`;
-  }
 
+  if (isMarried) {
+    partnerText = sex === 'm'
+      ? `${partnerName} is my wife`
+      : `${partnerName} is my husband`;
+  }
   return (
     <section className="Person">
-      <h2 className="Person__name">My name is {person.name}</h2>
-      {person.age && <p className="Person__age">I am {person.age}</p>}
+      <h2 className="Person__name">My name is {name}</h2>
+
+      {age && (
+        <p className="Person__age">I am {age}</p>
+      )}
+
       <p className="Person__partner">{partnerText}</p>
     </section>
   );
-}
+};
